e3b5445bb6bb6798a103030552e64591b17b616e
Luke Parker <luke@Lukes-MacBook-Pro.local>
2019-09-10 17:48:47 -0700

    implemented lots of stuff

--- space_man.py
+++ space_man.py
@@ -1,16 +1,8 @@
 import random
 
-
-guesses = list()
-
+word = list()
 
 def load_word():
-    '''
-    A function that reads a text file of words and randomly selects one to use as the secret word
-        from the list.
-    Returns: 
-           string: The secret word to be used in the spaceman guessing game
-    '''
     f = open('words.txt', 'r')
     words_list = f.readlines()
     f.close()
@@ -18,41 +10,38 @@ def load_word():
     words_list = words_list[0].split(' ') #comment this line out if you use a words.txt file with each word on a new line
     secret_word = random.choice(words_list)
     return secret_word
-    
+
+def find_indicies(str, find_char):
+    '''given str (string) and a find_char (char)
+    iterate through the string and return an array of indices 
+    where that find_char is present'''
+    indicies = []
+    for index in range(len(str)):
+        char = str[index]
+        if char == find_char:
+            indicies.append(index)
+    return indicies
 
 
-def is_word_guessed(secret_word, letters_guessed):
-    '''
-    A function that checks if all the letters of the secret word have been guessed.
-    Args:
-        secret_word (string): the random word the user is trying to guess.
-        letters_guessed (list of strings): list of letters that have been guessed so far.
-    Returns: 
-        bool: True only if all the letters of secret_word are in letters_guessed, False otherwise
-    '''
-    # TODO: Loop through the letters in the secret_word and check if a letter is not in lettersGuessed
-    pass
+def find_indicies_for_char(str, find_char):
+    '''alternate approach to find indicies '''
+    indicies = []
+    index = 0
+    for char in str:
+        if char == find_char:
+            indicies.append(index)
+        index += 1
+    return indicies
 
-def get_guessed_word(secret_word, letters_guessed):
-    '''
-    A function that is used to get a string showing the letters guessed so far in the secret word and underscores for letters that have not been guessed yet.
-    Args: 
-        secret_word (string): the random word the user is trying to guess.
-        letters_guessed (list of strings): list of letters that have been guessed so far.
-    Returns: 
-        string: letters and underscores.  For letters in the word that the user has guessed correctly, the string should contain the letter at the correct position.  For letters in the word that the user has not yet guessed, shown an _ (underscore) instead.
-    '''
-
-    #TODO: Loop through the letters in secret word and build a string that shows the letters that have been guessed correctly so far that are saved in letters_guessed and underscores for the letters that have not been guessed yet
-
-    pass
-
-
-def is_guess_in_word(secret_word):
-    user_input = input("Type in a letter")
-    #ltr_val = secret_word.find(user_input)
-    indices = [i for i, x in enumerate(secret_word) if x == user_input]
-    print(indices)   
+def find_indicies_enumerate(str, find_char):
+    '''alternate approach to find indicies using enumerate
+    enumerate allows us to keep track of both the index and 
+    the character we are up to '''
+    indicies = []
+    for index, char in enumerate(str):
+        if char == find_char:
+            indicies.append(index)
+    return indicies
 
 
 
@@ -60,37 +49,18 @@ def is_guess_in_word(secret_word):
 
 
 
+def user_input(prompt):
+    user_input = input(prompt)
 
-def spaceman(secret_word):
-    name = input("What is your name?")
-    print("Hello " + name + "welcome to spaceman!")
-    print("Guess which letters are in the word!")
-    #ltr_val = 0
-    print(secret_word)
-    is_guess_in_word(secret_word)
-   
-    
+def game():
+    secret_word = load_word()
+    blanks = ["_"] * len(secret_word)
+    lives = 7
 
+    user_input("Enter a letter") 
+    print(word)       
+                
+print("Welcome to space man! Try to guess the secret word by guessing different letters!")
+game()
 
-
-
-    #TODO: show the player information about the game according to the project spec
-
-    #TODO: Ask the player to guess one letter per round and check that it is only one letter
-
-    #TODO: Check if the guessed letter is in the secret or not and give the player feedback
-
-    #TODO: show the guessed word so far
-
-    #TODO: check if the game has been won or lost
-
-
-
-
-
-
-
-
-secret_word = load_word()
-spaceman(secret_word) 
 
