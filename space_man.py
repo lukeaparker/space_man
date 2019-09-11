7780ee9240b639346b7fec3a1a1265d9395ded88
Luke Parker <luke@Lukes-MacBook-Pro.local>
2019-09-10 19:01:55 -0700

    added error handling

--- space_man.py
+++ space_man.py
@@ -51,6 +51,14 @@ def find_indicies_enumerate(str, find_char):
 
 def user_input(prompt):
     user_input = input(prompt)
+    while len(user_input) != 1 or not user_input.isalpha():
+        # if the user input is one character
+        if len(user_input) != 1:
+            user_input = input("Please input one letter at a time: ")
+        # if it is not a letter
+        if not user_input.isalpha():
+            user_input = input("Please input a letter: ")
+
     return user_input
 
 def replace_blanks_with_letter(char, blanks, indicies):
@@ -70,16 +78,27 @@ def game():
     blanks = ["_"] * len(secret_word)
     lives = 7
     while lives > 0 and secret_word != ''.join(blanks):
-        guess = user_input("Enter a letter") 
+        guess = user_input("Enter a letter: ") 
         indicies = find_indicies(secret_word, guess)    
         #if the letter is found 
         if len(indicies) > 0: 
             #  replace blanks with character guessed
             blanks = replace_blanks_with_letter(guess, blanks, indicies)
             # afirm user for correct answer
-            print("Correct," , guess , "is in the word!")
+            print("Correct!" , guess , "is in the word!")
             print("".join(blanks))
             print("Lives:" , lives)
+            print("Guesses:")
+        # the guess was incorrect
+        else:
+            lives -= 1
+            print("Wrong!", guess , "is not in the word!")
+            print("".join(blanks))
+            print("Lives:" , lives)
+    if lives == 0:
+        print("Game over!!! The secret word was " + secret_word + ".")
+    else:
+        print("Congratulations! You won the game!")
 print("Welcome to space man! Try to guess the secret word by guessing different letters!")
 game()
 
