a746b3ed611dcd475c807c74c55afe1ddd548ac1
Luke Parker <luke@Lukes-MacBook-Pro.local>
2019-09-10 18:33:10 -0700

    update blanks

--- space_man.py
+++ space_man.py
@@ -51,15 +51,35 @@ def find_indicies_enumerate(str, find_char):
 
 def user_input(prompt):
     user_input = input(prompt)
+    return user_input
+
+def replace_blanks_with_letter(char, blanks, indicies):
+    '''given a char, a list of blanks, and a list of indicies
+    replace blanks with character at every index of indicies'''
+    # secret_word = 'hello'
+    # blanks = ['_','_','_','_','_',]
+    # indicies = [2,3]
+    # char = 'l'
+    for update_index in indicies:
+        # update blanks at index update_index w char
+        blanks[update_index] = char
+    return blanks 
 
 def game():
     secret_word = load_word()
     blanks = ["_"] * len(secret_word)
     lives = 7
-
-    user_input("Enter a letter") 
-    print(word)       
-                
+    while lives > 0 and secret_word != ''.join(blanks):
+        guess = user_input("Enter a letter") 
+        indicies = find_indicies(secret_word, guess)    
+        #if the letter is found 
+        if len(indicies) > 0: 
+            #  replace blanks with character guessed
+            blanks = replace_blanks_with_letter(guess, blanks, indicies)
+            # afirm user for correct answer
+            print("Correct," , guess , "is in the word!")
+            print("".join(blanks))
+            print("Lives:" , lives)
 print("Welcome to space man! Try to guess the secret word by guessing different letters!")
 game()
 
